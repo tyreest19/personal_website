a5638cd626f302da98bc92c51644d73687ec46ff
Tyree Stevenson <tyreeostevenson@gmail.com>
2016-12-09 19:57:05 -0500

    creating a one page site

--- main.py
+++ main.py
@@ -2,21 +2,12 @@ from flask import Flask
 from flask import render_template
 
 
-app = Flask(__name__,template_folder='templates',static_url_path = '/static')
+app = Flask(__name__,template_folder='template',static_url_path = '/static')
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
-@app.route("/resume")
-def resume():
-    return render_template("resume.html",)
-
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-@app.route("/experience")
-def experience():
-    return render_template("experience.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
